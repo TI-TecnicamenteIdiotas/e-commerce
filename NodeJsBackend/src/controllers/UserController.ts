8619fff1f51a248f397a4e0c2fe62bad98d15d00
Almir Jr <almirafjr.dev@gmail.com>
2021-10-22 23:05:43 -0300

    Add bad request to new login with missing fields in the body

--- NodeJsBackend/src/controllers/UserController.ts
+++ NodeJsBackend/src/controllers/UserController.ts
@@ -4,9 +4,9 @@ import { client } from '../services/DatabaseService.js';
 const router = Router();
 
 //	Returns specific login and password
-router.get('/login/:id', async (request, response) => {
-	response.json((await client.query('SELECT email, senha FROM usuarios WHERE id = $1',
-		[request.params.id])).rows);
+router.get('/login/:name', async (request, response) => {
+	response.json((await client.query('SELECT email, senha FROM usuarios WHERE nome = $1',
+		[request.params.name])).rows);
 });
 
 //	Returns already existed logins
@@ -17,8 +17,11 @@ router.post('/login', async (request, response) => {
 
 //	Registers a new login with password in database
 router.post('/register', async (request, response) => {
-	response.json((await client.query('INSERT INTO usuarios(email, senha) VALUES ($1, $2);',
-		[request.body.email, request.body.password])).rows);
+	if (!request.body.name || !request.body.email || !request.body.password) {
+		response.sendStatus(400);
+	}
+	response.json((await client.query('INSERT INTO usuarios(nome, email, senha) VALUES ($1, $2, $3);',
+		[request.body.name, request.body.email, request.body.password])).rows);
 });
 
 export default router;
