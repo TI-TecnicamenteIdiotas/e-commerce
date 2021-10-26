bf5659070029cf271a20e812b1a8316395db0ef5
Almir Jr <almirafjr.dev@gmail.com>
2021-10-26 20:32:43 -0300

    Add in code documentation

--- NodeJsBackend/src/index.ts
+++ NodeJsBackend/src/index.ts
@@ -4,21 +4,28 @@ import { connectToDatabase } from './services/DatabaseService.js';
 import AdminController from './controllers/AdminController.js';
 import UserController from './controllers/UserController.js';
 
+/** Instance of a Express server */
 const app = Express();
+
+//Inject module on server instance
 app.use(cors());
 app.use(Express.json());
 
+//Estabilish database connection
 await connectToDatabase();
 
+//Inject controllers with route
 app.use('/admin', AdminController);
 app.use('/user', UserController);
 
-//	Default route api response
+//Default route api response
 app.get('/', async (request, response) => {
 	response.send('<h1>E-commerce Api</h1>');
 });
 
-/** Serves Api on standard 8080 port */
+//Serves Api on standard 8080 port
 app.listen(8080, () => {
 	console.log('Server open and listening on http://localhost:8080');
 });
+
+export default app;
