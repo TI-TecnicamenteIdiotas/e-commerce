1a7765c0d180053e32d96bf5fb1d7812effec391
Almir Jr <almirafjr.dev@gmail.com>
2021-10-28 02:12:40 -0300

    Add backend pagination support

--- NodeJsBackend/src/controllers/DigitalProductController.ts
+++ NodeJsBackend/src/controllers/DigitalProductController.ts
@@ -10,9 +10,13 @@ router.get('/byName/:productName', async (request, response) => {
 		[request.params.productName.replace(/_/g, " ")])).rows);
 });
 
-//	Returns All digital products data in Json format
+//	Returns All digital products data in Json format with pagination treatment
 router.get('/allProducts', async (request, response) => {
-	response.json((await client.query('SELECT * FROM mercadorias_digitais')).rows);
+	let itemsPerPage = request.query.itemsPerPage ? Number(request.query.itemsPerPage) : 5;
+	let offset = request.query.page ? ((Number(request.query.page) - 1) * itemsPerPage) : 0;
+
+	response.json((await client.query('SELECT * FROM mercadorias_digitais LIMIT $1 OFFSET $2',
+		[itemsPerPage, offset])).rows);
 });
 
 export default router;
